5e786e0be4722788a1311e04eae03c5e576ec4a7
Sidnei Oliveira | Tecnologia | Via Expressa <sidnei.oliveira@viaexpressa.com>
2025-02-07 18:57:58 -0300

    mapeando a entidade autor

--- src/main/java/io/github/cursoSpring/libraryapi/config/DataBaseConfiguration.java
+++ src/main/java/io/github/cursoSpring/libraryapi/config/DataBaseConfiguration.java
@@ -1,4 +1,52 @@
-package config;
+package io.github.cursoSpring.libraryapi.config;
 
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class DataBaseConfiguration {
+
+    @Value(("${spring.datasource.url}"))
+    String url;
+    @Value(("${spring.datasource.username}"))
+    String username;
+    @Value(("${spring.datasource.password}"))
+    String password;
+    @Value(("${spring.datasource.driver-class-name}"))
+    String driver;
+
+    //@Bean
+    public DataSource dataSource(){ // não recomendado para produção
+
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    @Bean
+    public DataSource hikariDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10); // vai liberar 10 conexoes
+        config.setMinimumIdle(1); // tamanho inicial do pool
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000); //timeout para conseguir nova conexao
+        config.setConnectionTestQuery("select 1"); // testar se está conectando com o banco
+        return new HikariDataSource(config);
+    }
 }
---
+++ src/main/java/io/github/cursoSpring/libraryapi/model/Autor.java
@@ -0,0 +1,64 @@
+package io.github.cursoSpring.libraryapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable= false) // atributos como length nao são necessarios pois o banco já delimita é opcional
+    private String nome;
+
+    @Column(name= "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+
+}
