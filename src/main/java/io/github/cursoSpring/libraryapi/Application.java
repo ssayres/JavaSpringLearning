e8d2b936c55e250faade5f8e4b4ea09696e9b22e
Sidnei Oliveira | Tecnologia | Via Expressa <sidnei.oliveira@viaexpressa.com>
2025-02-12 19:03:24 -0300

    utilizando metodos crud, ligando os Repositorys criados com o banco de dados - através do JPA REPOSITORY

--- src/main/java/io/github/cursoSpring/libraryapi/Application.java
+++ src/main/java/io/github/cursoSpring/libraryapi/Application.java
@@ -1,14 +1,31 @@
 package io.github.cursoSpring.libraryapi;
 
+import io.github.cursoSpring.libraryapi.model.Autor;
+import io.github.cursoSpring.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+		var context =SpringApplication.run(Application.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
 
+		exemploSalvarRegistro(repository);
+	}
+	public static void exemploSalvarRegistro(AutorRepository autorRepository){
+		Autor autor = new Autor();
+		autor.setNome("José");
+		autor.setNacionalidade("Brasileira");
+		autor.setDataNascimento(LocalDate.of(1950,1,31));
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor Salvo " + autorSalvo);
+
+
+	}
 
 }
--- src/main/java/io/github/cursoSpring/libraryapi/model/Autor.java
+++ src/main/java/io/github/cursoSpring/libraryapi/model/Autor.java
@@ -1,44 +1,110 @@
 package io.github.cursoSpring.libraryapi.model;
 
 
+
 import jakarta.persistence.*;
+
 import lombok.Getter;
+
 import lombok.Setter;
+import lombok.ToString;
+
 
 import java.time.LocalDate;
-import java.util.List;
+
 import java.util.UUID;
 
+
 @Entity
+
 @Table(name = "autor", schema = "public")
+
 @Getter
+
 @Setter
+
+@ToString
+
 public class Autor {
 
-    @Getter
-    @Setter
+
     @Id
+
     @Column(name = "id")
+
     @GeneratedValue(strategy = GenerationType.UUID)
+
     private UUID id;
 
-    @Setter
-    @Getter
+
     @Column(name = "nome", length = 100, nullable= false) // atributos como length nao são necessarios pois o banco já delimita é opcional
+
     private String nome;
 
-    @Setter
-    @Getter
+
     @Column(name= "data_nascimento", nullable = false)
+
     private LocalDate dataNascimento;
 
-    @Setter
-    @Getter
+
     @Column(name = "nacionalidade", length = 50, nullable = false)
+
     private String nacionalidade;
 
-    @OneToMany(mappedBy = "autor") // dentro da entidade livro - a relação é através da propriedade autor
-    private List<Livro> livros;
 
+    public UUID getId() {
+
+        return id;
+
+    }
+
+
+    public void setId(UUID id) {
+
+                this.id = id;
+
+    }
+
+
+    public String getNome() {
+
+        return nome;
+
+    }
+
+
+    public void setNome(String nome) {
+
+                this.nome = nome;
+
+    }
+
+
+    public LocalDate getDataNascimento() {
+
+        return dataNascimento;
+
+    }
+
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+
+                this.dataNascimento = dataNascimento;
+
+    }
+
+
+    public String getNacionalidade() {
+
+        return nacionalidade;
+
+    }
+
+
+    public void setNacionalidade(String nacionalidade) {
+
+                this.nacionalidade = nacionalidade;
+
+    }
 
 }
