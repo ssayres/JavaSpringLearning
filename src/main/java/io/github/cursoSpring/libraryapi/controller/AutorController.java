5a731bb52a11d2a589ef1e545dc27d339508749b
ssayres <ssayres@outlook.com>
2025-03-13 14:31:40 -0300

    iniciando contrado de API - criando o primeiro controller

---
+++ src/main/java/io/github/cursoSpring/libraryapi/controller/AutorController.java
@@ -0,0 +1,11 @@
+package io.github.cursoSpring.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autores")
+// host = http://localhost:8080/autores
+public class AutorController {
+
+}
