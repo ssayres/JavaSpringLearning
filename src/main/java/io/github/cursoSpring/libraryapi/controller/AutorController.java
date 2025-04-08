a868ecb0d63203b8e78e1786870e3ed61760e9de
ssayres <ssayres@outlook.com>
2025-04-07 22:31:24 -0300

    Pesquisa de autores com filtro

--- src/main/java/io/github/cursoSpring/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursoSpring/libraryapi/controller/AutorController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("autores")
@@ -65,6 +67,20 @@ public class AutorController {
         service.deletar(autorOptional.get());
         return  ResponseEntity.noContent().build();
     }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(@RequestParam(value = "nome", required = false) String nome, @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream().map(autor -> new AutorDTO(
+                autor.getId(),
+                autor.getNome(),
+                autor.getDataNascimento(), autor.getNacionalidade()
+                        )
+                ).collect(Collectors.toList());
+                return ResponseEntity.ok(lista);
+    }
+
 
 
 }
--- src/main/java/io/github/cursoSpring/libraryapi/service/AutorService.java
+++ src/main/java/io/github/cursoSpring/libraryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import io.github.cursoSpring.libraryapi.model.Autor;
 import io.github.cursoSpring.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +27,22 @@ public class AutorService {
 
     public void deletar(Autor autor){
         repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+
+        if(nome != null && nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if(nome != null){
+            return repository.findByNome(nome);
+        }
+        if(nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+        return repository.findAll();
+
     }
 
 }
