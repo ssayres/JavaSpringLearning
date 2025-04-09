92ab5a86aa568697199929bf76feaa9843278340
Sidnei Oliveira | Tecnologia | Via Expressa <sidnei.oliveira@viaexpressa.com>
2025-04-09 18:57:40 -0300

    método de PUT de autores

--- src/main/java/io/github/cursoSpring/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursoSpring/libraryapi/controller/AutorController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("autores")
@@ -65,6 +67,38 @@ public class AutorController {
         service.deletar(autorOptional.get());
         return  ResponseEntity.noContent().build();
     }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(@RequestParam(value = "nome", required = false) String nome, @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream().map(autor -> new AutorDTO(
+                                autor.getId(),
+                                autor.getNome(),
+                                autor.getDataNascimento(), autor.getNacionalidade()
+                        )
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if(autorOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        var autor = autorOptional.get();
+        autor.setNacionalidade((dto.nacionalidade()));
+        autor.setDataNascimento((dto.dataNascimento()));
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
--- src/main/java/io/github/cursoSpring/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/cursoSpring/libraryapi/repository/AutorRepository.java
@@ -3,9 +3,14 @@ package io.github.cursoSpring.libraryapi.repository;
 import io.github.cursoSpring.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 //Component e Repository são opcionais - o Repositorio já entende que se trata de um componente repository pois estende do JPA REPOSITORY
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 
 }
