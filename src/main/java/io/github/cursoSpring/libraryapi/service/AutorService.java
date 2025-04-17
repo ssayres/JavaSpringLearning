551351aa8b32336aab55f6c0e7eaf53c30416121
Sidnei Oliveira | Tecnologia | Via Expressa <sidnei.oliveira@viaexpressa.com>
2025-04-17 18:47:31 -0300

    Injetando com requiredargs

--- src/main/java/io/github/cursoSpring/libraryapi/service/AutorService.java
+++ src/main/java/io/github/cursoSpring/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import io.github.cursoSpring.libraryapi.model.Autor;
 import io.github.cursoSpring.libraryapi.repository.AutorRepository;
 import io.github.cursoSpring.libraryapi.repository.LivroRepository;
 import io.github.cursoSpring.libraryapi.validator.AutorValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +13,18 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor // serve como um construtor ara os componentes final automaticamente
 public class AutorService {
 
-    private final AutorRepository repository;
-    private final AutorValidator validator ;
-    private final LivroRepository livroRepository;
+    private  final AutorRepository repository;
+    private  final AutorValidator validator ;
+    private  final LivroRepository livroRepository;
 
-    public AutorService(AutorRepository repository, AutorValidator validator, LivroRepository livroRepository){
-        this.repository = repository;
-        this.validator = validator;
-        this.livroRepository = livroRepository;
-    }
+   // public AutorService(AutorRepository repository, AutorValidator validator, LivroRepository livroRepository){
+    //    this.repository = repository;
+    //    this.validator = validator;
+    //    this.livroRepository = livroRepository;
+    //}
 
     public Autor salvar(Autor autor){
         validator.validar(autor);
